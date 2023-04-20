2fca2c4f44a58c986bdefab2347e4264fffbbf62
root <dorcasauma89@gmail.com>
2023-04-20 09:53:21 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main() { printf("In the midst of winter, I found there was, within me,\n an invincible summer.\n- Albert Camus\n"); return 0; }
+int main() { printf("with proper grammar, but the outcome is a piece of art,\n"); return 0; }
