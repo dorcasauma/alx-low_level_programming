fc37b8ec426edad042ecf94d013490f34f9a8273
root <dorcasauma89@gmail.com>
2023-04-20 10:56:51 -0700

    printif

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,7 @@
-/*a C program that prints exactly with proper grammar,
- * but the outcome is a piece of art,, followed by a new line.*/
+/*
+ * a C program that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+*/
 #include <stdio.h>
 int main(void)
 {
