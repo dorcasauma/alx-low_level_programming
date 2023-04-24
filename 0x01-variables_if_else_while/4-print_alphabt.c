04c137e34c0166912877e44441f7b190ceb86a6b
mwaaas <francismwangi152@gmail.com>
2023-04-24 21:00:23 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+if (letter != 'e' && letter != 'q')
+
+putchar(letter);
+}
+
+letter++;
+}
+
+putchar('\n');
+
+return (0);
