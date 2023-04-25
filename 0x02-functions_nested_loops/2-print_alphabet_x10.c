e384bee40484461fc63fc626013f2c504ad6867a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-25 16:40:40 +0300

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@
  * Return: Always 0.
  */
 
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
     char c;
     int i;
@@ -15,8 +15,8 @@ void print_alphabet_x10(void)
     {
         for (c = 'a'; c <= 'z'; c++)
         {
-            _putchar(c);
+            putchar(c);
         }
-        _putchar('\n');
+        putchar('\n');
     }
 }
