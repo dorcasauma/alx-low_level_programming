92a525972768c639bdfe766515010e40b1324c76
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-25 16:36:13 +0300

    print10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet - Prints the lowercase alphabet followed by a new line.
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+    char c;
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+    }
+}
