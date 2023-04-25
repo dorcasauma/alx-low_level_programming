44cb86b02930575f3881ecf3abc6e8a11b7068ea
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-25 20:17:38 +0300

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * checks is a character is islower.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+if (islower(c))
+{
+return 1;
+}
+else {
+return 0;
+}
+}
