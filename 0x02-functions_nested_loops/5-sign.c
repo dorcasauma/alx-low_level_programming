c970b3cc3ce437d2f6feaea38f97bb7f9f2f31da
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-26 16:10:59 +0300

    k

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,18 +7,21 @@
  *
  * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero.
  */
-int print_sign(int n){
-
-    int r;
-
-    r = print_sign(n);
-    printf("%d\n", r);
-    r = print_sign(0);
-    printf("%d\n", r);
-    r = print_sign(0xff);
-    printf("%d\n", r);
-    r = print_sign(-1);
-    printf("%d\n", r);
-
-    return (0);
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+putchar('0');
+return (0);
+}
+else
+{
+putchar('-');
+return (-1);
+}
 }
