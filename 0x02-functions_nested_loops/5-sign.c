7c682b3724249655f292aa6d960e9b05b9f252f3
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-26 16:17:38 +0300

    hg

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-printf('+');
+printf("%c", '+');
 return (1);
 }
 else if (n == 0)
 {
-printf('0');
+printf("%c", '0');
 return (0);
 }
 else
 {
-printf('-');
+printf("%c", '-');
 return (-1);
 }
 }
