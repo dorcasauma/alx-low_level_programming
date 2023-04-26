c9fae69960d2bcefa6756b141c58fac1ff7ba46f
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-26 20:50:15 +0300

    s

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * int print_last_digit - prints the last digit of a number
+ *
+ * @int: the integer to print
+ * Return: 0 prints the last digit of a number
+ *
+ */
+
+int print_last_digit(int r)
+{
+int k =  r%10;
+printf(" %d", k);
+
+}
