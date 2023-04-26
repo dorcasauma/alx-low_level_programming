d165688171a8a533f7fbc92489a100b54fe985c1
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-26 16:21:25 +0300

    hgh

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,21 +7,18 @@
  *
  * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero.
  */
-int print_sign(int n)
+void print_sign(int n)
 {
 if (n > 0)
 {
 printf("%c %d", '+', n);
-return (1);
 }
 else if (n == 0)
 {
 printf("%c", '0');
-return (0);
 }
 else
 {
 printf("%c", '-');
-return (-1);
 }
 }
