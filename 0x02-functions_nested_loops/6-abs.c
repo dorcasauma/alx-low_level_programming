a821d31d712e3493afc8800b731e50ce834a01eb
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-26 19:39:46 +0300

    new file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * int _abs- absolute value of an integer
+ *
+ * @int: the number to check
+ *
+ * Return:absolute value of an integer
+ */
+
+int _abs(int r)
+{
+    return abs(r);
+}
