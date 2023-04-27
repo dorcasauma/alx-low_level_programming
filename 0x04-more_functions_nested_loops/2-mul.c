9be36663961000b5ee518fba97acc86ae4b1f021
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-27 12:04:42 +0300

    lkm

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * int mul - multiplies two integers..
+ *
+ *@a,@b: character to be multiplied.
+ *Return:@a*@b
+ */
+
+int mul(int a, int b)
+{
+return (a*b);
+}
