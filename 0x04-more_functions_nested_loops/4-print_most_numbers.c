470efcb801d8f6caef3a16bcea570d3531f47911
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-27 15:18:50 +0300

    zx

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,13 +9,12 @@
 void print_most_numbers(void)
 {
 char c;
-for (c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++){
 if (c == 2 || c == 4)
 {
 continue;
 }
-{
 putchar(c);
-}
 putchar('\n');
+}
 }
