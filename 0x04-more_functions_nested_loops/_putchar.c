2fc06d2d9d869cc08de4c4601e62139fa85fbeb3
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-27 14:15:32 +0300

    jgfg

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * Checks if a character is a digit (0-9)
+ * @param c The character to check
+ * @return Non-zero value if the character is a digit, 0 otherwise
+ */
+
+int _isdigit(int c)
+{
+if (isdigit(c))
+return (1);
+else
+{
+return (0);
+}
+}
