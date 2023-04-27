5f6b592f7cd629856436f81ea45b9f300425920d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-27 14:18:30 +0300

    dss

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,11 @@
  * @return zero value
  */
 
-void print_numbers(void) {
-    char c;
-    for (c = '0'; c <= '9'; c++) {
-        _putchar(c);
-    }
-    _putchar('\n');
+void print_numbers(void)
+{
+char c;
+for (c = '0'; c <= '9'; c++) {
+putchar(c);
+}
+putchar('\n');
 }
