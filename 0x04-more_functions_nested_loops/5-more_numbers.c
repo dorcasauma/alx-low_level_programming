f24a770470d5a06944df1c52c0173445cac90c8e
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-28 05:19:59 +0300

    fix styling

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- *prints 10 times the numbers, from 0 to 14, followed by a new line.
+ *more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
  * @return zero value
  */
 
@@ -14,14 +14,14 @@ while (k <10)
 int c;
 for (c = 0; c <= 14; c++)
 {
-    int d = c;
-    if (c > 9){
-        int first = c/10;
-        int second = c % 10;
-        putchar(first + '0');
-        d = second;
-    }
-    putchar(d + '0');
+int d = c;
+if (c > 9){
+int first = c/10;
+int second = c % 10;
+putchar(first + '0');
+d = second;
+}
+putchar(d + '0');
 }
 putchar('\n');
 k += 1;
