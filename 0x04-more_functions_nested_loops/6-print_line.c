b082fe0cb4efc5f034c5e989d38ca3b738ae0820
DORCAS AUMA <dorcasauma89@gmail.com>
2023-04-28 11:54:59 +0300

    dd

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,11 +7,11 @@
  */
 
 void print_line(int n) {
+    int i;
     if (n <= 0) {
         putchar('\n');
         return;
     }
-    int i;
     for (i = 0; i < n; i++) {
         putchar('_');
     }
