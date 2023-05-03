84e4e8d20dcd0c306fa5a0190c02cbab2fecdbb4
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-03 05:51:44 +0300

    mn

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,11 +13,13 @@ void rev_string(char *s)
 char *start = s;
 char *end = s;
 char tmp;
-while (*end != '\0') {
+while (*end != '\0')
+
 end++;
-}
+
 end--;
-while (start < end) {
+while (start < end)
+{
 tmp = *start;
 *start = *end;
 *end = tmp;
