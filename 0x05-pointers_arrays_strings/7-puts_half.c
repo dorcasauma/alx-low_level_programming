3f3ade5b53ee9b9d486e48d5ffe9418c40a8e737
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-03 21:34:35 +0300

    fgg

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * puts_half - Prints half of a string.
+ * @str: Pointer to the string to be printed.
+ *
+ * Return: void.
+ */
+void puts_half(char *str)
+{
+int len = strlen(str);
+int start = len / 2;
+if (len % 2 != 0)
+{
+start += 1;
+}
+}
