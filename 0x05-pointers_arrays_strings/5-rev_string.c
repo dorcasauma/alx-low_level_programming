fee36e50d1d5d44728375488e1ba76e0be4f7451
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-03 05:49:38 +0300

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * print_rev - reverses a string
+ * @s: is the string to be reversed
+ *
+ *
+ *reverses a string
+ *
+ */
+
+void rev_string(char *s)
+{
+char *start = s;
+char *end = s;
+char tmp;
+while (*end != '\0') {
+end++;
+}
+end--;
+while (start < end) {
+tmp = *start;
+*start = *end;
+*end = tmp;
+start++;
+end--;
+}
+}
+
