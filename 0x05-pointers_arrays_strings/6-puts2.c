1422f97700c7e90effcf72e607bda0061eb2a7f3
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-03 21:24:45 +0300

    m,

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,9 +2,9 @@
 /**
  * puts2-prints every other character
  * \n of a string, starting with the first character,
- * followed by a new line.
+ * \n followed by a new line.
  *
- * @str - the string to point
+ * @str:the string to point
  *Return: void
  */
 void puts2(char *str)
