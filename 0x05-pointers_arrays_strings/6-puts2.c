e550ced33724cf8ec2e4e90166005bf8d8dfb04a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-03 21:14:13 +0300

    jj

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * puts2-prints every other character of a string, starting with the first character,
+ * puts2-prints every other character
+ * \n of a string, starting with the first character,
  * followed by a new line.
  *
  * @param str - pointer to a null-terminated string to be printed
