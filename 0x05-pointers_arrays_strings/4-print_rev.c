4ebf48875434e56157968102fe98d2f6e206610a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-03 17:15:18 +0300

    ty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,16 @@
 void print_rev(char *s)
 {
 int len = 0;
-
-/* Find the length of the string */
-while (s[len] != '\0')
+while (*s != '\0')
 {
 len++;
+s++;
 }
+s--;
+while (len > 0){
+putchar(*s);
+s--;
+len--;
 }
-
+putchar('\n');
+}
