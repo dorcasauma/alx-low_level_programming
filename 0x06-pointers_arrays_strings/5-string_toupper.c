008a8bb1dae60a4b4087e068b56c7e2c7cc5500f
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 16:17:06 +0300

    uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - Changes all lowercase letters of a \n  string to uppercase.
+ * @str: Pointer to the string to be converted.
+ *
+ * This function takes a string pointed to by @str and changes all \n lowercase
+ * letters to uppercase. It then returns a pointer to the converted string.
+ */
+char *string_toupper(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+}
+}
+return (str);
+}
