4394511adbc458be16342e1a633f77557ced4d06
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 05:57:36 +0300

    vvv

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+/**
+ * print_array - Prints n elements of an array of integers, followed by a new line.
+ * @a: Pointer to the first element of the array to be printed.
+ * @n: Number of elements of the array to be printed.
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+
+for (i = 0; i < n; i++)
+{
+
+if (i < n - 1)
+{
+printf("%d, ", a[i]);
+}
+
+else
+{
+printf("%d\n", a[i]);
+}
+}
+}
