cdb19e327141b327d9b112eb99f1ac992654b36f
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 17:51:48 +0300

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+/**
+ * rot13 - Encodes a string using rot13
+ * @s: Pointer to the string to be encoded
+ *
+ * Return: Pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+  char *p = s;
+  int i;
+
+  while (*s)
+  {
+i = (*s >= 'a' && *s <= 'z') ? (*s - 'a') : (*s - 'A');
+if (i >= 0 && i < 26)
+*s = (*s + 13 > 'z' || (*s + 13 > 'Z' && *s <= 'Z')) ? (*s - 13) : (*s + 13);
+s++;
+}
+return (p);
+}
