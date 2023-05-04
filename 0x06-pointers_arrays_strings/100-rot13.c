0921cb4df07a0e23b86dce9abc81703c51b250d8
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 17:53:29 +0300

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,11 +6,11 @@
  */
 char *rot13(char *s)
 {
-  char *p = s;
-  int i;
+char *p = s;
+int i;
 
-  while (*s)
-  {
+while (*s)
+{
 i = (*s >= 'a' && *s <= 'z') ? (*s - 'a') : (*s - 'A');
 if (i >= 0 && i < 26)
 *s = (*s + 13 > 'z' || (*s + 13 > 'Z' && *s <= 'Z')) ? (*s - 13) : (*s + 13);
