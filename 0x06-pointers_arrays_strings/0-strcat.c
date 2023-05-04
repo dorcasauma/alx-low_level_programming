a15fb3789c91b8d978f9b3ffba6340511d885bad
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 15:15:14 +0300

    0-strcat code

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+/**
+ * *_strcat - Concatenates the string pointed to by `src` \n to the end of the string
+ * pointed to by `dest`.
+ *
+ * This function overwrites the null \n byte ('\0') at the end of `dest`
+ * \n and then copies the characters of `src` to the end of `dest`. It
+ * \n then adds a new null byte at the end to terminate the resulting
+ * string.
+ *
+ * @param dest A pointer to the destination string.
+ * @param src A pointer to the source string.
+ *
+ * @return A pointer to the resulting string (`dest`).
+ */
+char *_strcat(char *dest, char *src)
+{
+char *dest_end = dest;
+char *src_start = src;
+
+while (*dest_end != '\0') {
+dest_end++;
+}
+
+while (*src != '\0')
+{
+*dest_end++ = *src++;
+}
+
+*dest_end = '\0';
+return dest;
+}
