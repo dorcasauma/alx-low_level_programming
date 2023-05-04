b501ffa0b7af155e2f12a6c63897b4ce92eaf442
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 16:25:36 +0300

    capititalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: Pointer to the string to be capitalized.
+ *
+ * This function takes a string pointed to by @str and \n capitalizes the first
+ * letter of each word. It then returns a pointer to the \n capitalized string.
+ * Words are delimited by \n whitespace characters and punctuation marks.
+ */
+char *cap_string(char *str)
+{
+int i;
+{
+str[0] = str[0] - 32;
+
+for (i = 1; str[i] != '\0'; i++)
+{
+
+if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n'
+|| str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.'
+|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"'
+|| str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{'
+|| str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
+{
+str[i] = str[i] - 32; // Subtract 32 to convert lowercase to uppercase
+}
+}
+
+return (str);
+}
