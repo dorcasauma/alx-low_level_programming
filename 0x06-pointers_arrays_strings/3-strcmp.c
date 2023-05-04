e102417fbc85cdc0ae02e57822fd1864f952f8a2
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 16:03:41 +0300

    jjj

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -19,7 +19,8 @@ while (s1[i] != '\0' && s2[i] != '\0')
 if (s1[i] < s2[i])
 {
 return -1;
-} else if (s1[i] > s2[i])
+}
+else if (s1[i] > s2[i])
 {
 return 1;
 }
@@ -32,7 +33,8 @@ return 0;
 } else if (s1[i] == '\0')
 {
 return -1;
-} else
+}
+else
 {
 return 1;
 }
