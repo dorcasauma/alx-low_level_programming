cb58068b744902c5a87eb8f6c8aaad01b577273d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-04 15:33:40 +0300

    1-strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,33 @@
+#include <string.h>
+
+/**
+ * *_strncat - Concatenates the first n characters of the string \n pointed to by `src`
+ * to the end of the string \n pointed to by `dest`.
+ *
+ * This function overwrites \n the null byte ('\0') at the end of `dest`
+ * and then copies at \n  most `n` characters of `src` to the end of `dest`.
+ * If `src` contains `n` or more \n characters, the resulting string will
+ * not be null-terminated.
+ *
+ * @param dest A pointer to the destination string.
+ * @param src A pointer to the source string.
+ * @param n The maximum number of characters to copy from `src`.
+ *
+ * @return A pointer to the resulting string (`dest`).
+ */
+char *_strncat(char *dest, const char *src, size_t n)
+{
+char *dest_end = dest;
+
+while (*dest_end != '\0')
+{
+dest_end++;
+}
+while (*src != '\0' && n-- > 0)
+{
+*dest_end++ = *src++;
+}
+*dest_end = '\0';
+return dest;
+}
+
