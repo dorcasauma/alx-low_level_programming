5d6cc5a2b50534602d3decfb4c678ea63990279c
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-05 14:48:56 +0300

    correction

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,11 +7,12 @@
  */
 int main(void)
 {
-int a[5] = {0, 1, 2,};
-int *p;
 
-p = &a[2];
-*p = 98;
-printf("a[2] = %d\n", *p); // prints "a[2] = 98"
+int a[] = { 0, 1, 2 };
+
+int *p = a;
+
+printf("a[2] = %d\n", *(p + 2) = 98);
+
 return (0);
 }
