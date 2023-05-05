a159cbe3c634272f306bc750949a4f489532dd99
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-05 15:02:37 +0300

    correction

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+int row, col, res;
+
+for (row = 0; row <= 9; row++)
+{
+for (col = 0; col <= 9; col++)
+{
+res = row * col;
+if (col == 0)
+{
+_putchar('0');
+}
+else if (res < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(res + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(res / 10 + '0');
+_putchar(res % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
