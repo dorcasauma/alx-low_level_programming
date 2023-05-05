4970cd1d94f4abc5c5aafdfe8e3aded91054c36d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-05 14:39:32 +0300

    correction

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,35 @@
-/**
- * print_number - Prints an integer using _putchar function.
+/*
+ * print_number - print an integer to stdout
+ * @n: the integer to print
  *
- * @n: The integer to print.
+ * This function prints the integer @n to stdout using only the _putchar function.
  */
 void print_number(int n)
 {
-unsigned int num;
 
-if (n < 0)
-{
+int digit, divisor;
+
+/* Handle negative numbers */
+if (n < 0) {
 _putchar('-');
-num = -n;
-}
-else
-{
-num = n;
+n = -n;
 }
 
-if (num / 10) 
+/* Determine the divisor to get the leftmost digit of @n */
+for (divisor = 1; divisor <= n / 10; divisor *= 10)
 {
-        print_number(num / 10);
+/* Do nothing */
 }
 
-    _putchar((num % 10) + '0');
+/* Print each digit in @n */
+while (divisor > 0)
+{
+digit = n / divisor;
+_putchar('0' + digit);
+n -= digit * divisor;
+divisor /= 10;
+}
+
+/* Print a newline character */
+_putchar('\n');
 }
