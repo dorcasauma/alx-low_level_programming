9a35ff43eddeee8f0106c95112696dea351d3059
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-05 14:53:22 +0300

    correction

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  *
  *@c: the character to check
  *
- *Returns: 1 if c is a letter, lowercase or uppercase 0 otherwise.
+ * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise.
  */
 int _isalpha(int c)
 {
