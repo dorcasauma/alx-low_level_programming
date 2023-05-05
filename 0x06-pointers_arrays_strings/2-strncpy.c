822cffb7a40b733db72ddf346a1e8326576b1c61
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-05 14:25:22 +0300

    correction

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,18 +1,20 @@
 #include <string.h>
 
 /**
- *_strncpy - Copies at most `n` characters \n from the string pointed to by `src`
- * (including the terminating \n null character) into the array pointed to
- * by `dest`.
+ * _strncpy - Copies at most `n` characters from the
+ * string pointed to by `src`
+ * (including the terminating null character)
+ * into the array pointed to by `dest`.
  *
- * If the length of `src` is less than \n `n`, the remaining characters in
+ * If the length of `src` is less than  `n`,
+ *  the remaining characters in
  * the destination array are filled with null bytes ('\0').
  *
  * @param dest A pointer to the destination array.
  * @param src A pointer to the source string.
  * @param n The maximum number of characters to copy.
- *
  * @return A pointer to the destination array (`dest`).
+ * @return _strncpy A pointer to the destination array (`dest`)
  */
 char *_strncpy(char *dest, const char *src, size_t n)
 {
