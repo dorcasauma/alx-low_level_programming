ff3a12f05d28a3ae603a1e175452b7e1e7643d28
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-05 14:10:24 +0300

    corrections

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,41 +1,27 @@
-#include <string.h>
-
-/**
- * _strcmp - Compares the string pointed to by `s1` to the \n string pointed to by `s2`.
+/*
+ * _strcmp - compare two strings
+ * @s1: pointer to first string
+ * @s2: pointer to second string
  *
- * @param s1 A pointer to the first string to compare.
- * @param s2 A pointer to the second string to compare.
- *
- * @return An integer less than, equal to, or greater than zero if `s1`
- *         \n is found, respectively, to be less than, to match, or be greater
- *         than `s2`.
+ * This function compares the two
+ * strings pointed to by s1 and s2,
+ * an integer value based on their order.
+ * The function returns 0 if the strings
+ * are equal, a negative value
+ * if s1 is less than s2, and a positive value if
+ * s1 is greater than s2.
  */
-int _strcmp(const char *s1, const char *s2)
+int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-
-while (s1[i] != '\0' && s2[i] != '\0')
+while (s1[i] == s2[i])
 {
-if (s1[i] < s2[i])
+if (s1[i] == '\0')
 {
-return -1;
-}
-else if (s1[i] > s2[i])
-{
-return 1;
+return (0);
 }
 i++;
 }
 
-if (s1[i] == '\0' && s2[i] == '\0')
-{
-return 0;
-} else if (s1[i] == '\0')
-{
-return -1;
-}
-else
-{
-return 1;
-}
+return (int) (s1[i] - s2[i]);
 }
