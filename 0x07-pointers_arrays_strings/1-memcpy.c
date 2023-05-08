d8773f3b067433ba32ab11140e69fceac6f75d0e
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 15:12:07 +0300

    fix code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,7 +14,8 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned char *dest_ptr = (unsigned char*) dest;
 unsigned char *src_ptr = (unsigned char*) src;
-for (unsigned int i = 0; i < n; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
 dest_ptr[i] = src_ptr[i];
 }
