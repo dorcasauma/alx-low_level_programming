36d5064f050ed4514760ef3ce7fd4efdc02029db
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-09 00:00:17 +0300

    another betty fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * *_strchr - Locates a character in a string.
+ * _strchr - Locates a character in a string.
  *
  * @s: A pointer to the string to search
  * @c: The character to search for
