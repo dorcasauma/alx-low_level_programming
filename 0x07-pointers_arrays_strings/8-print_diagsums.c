e7b354ebaf68e0dc74d8e69bb03ee9be26920490
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 23:02:51 +0300

    fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,8 +10,8 @@ void print_diagsums(int *a, int size)
 int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < size; i++)
 {
-sum1 += *(a + i * size + i);
-sum2 += *(a + i * size + (size -1 - i));
+sum1 += * (a + i * size + i);
+sum2 += * (a + i * size + (size -1 - i));
 printf("Diagonal sum 1: %d\n", sum1);
 printf("Diagonal sum 2: %d\n", sum2);
 }
