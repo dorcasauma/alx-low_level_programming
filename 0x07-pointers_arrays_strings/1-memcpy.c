794fed3a0d2fa3d2aa1dcdbfe0612a5ca01a2178
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 15:14:53 +0300

    betty fix

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,22 +4,22 @@
 /**
  * _memcpy - Copies n bytes from memory area src to memory area dest.
  *
- * @param dest A pointer to the destination memory area
- * @param src A pointer to the source memory area
- * @param n The number of bytes to copy
+ * @dest A pointer to the destination memory area
+ * @src A pointer to the source memory area
+ * @n The number of bytes to copy
  *
  * @return A pointer to the destination memory area
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned char *dest_ptr = (unsigned char*) dest;
-unsigned char *src_ptr = (unsigned char*) src;
+unsigned char *dest_ptr = (unsigned char *) dest;
+unsigned char *src_ptr = (unsigned char *) src;
 unsigned int i;
 for (i = 0; i < n; i++)
 {
 dest_ptr[i] = src_ptr[i];
 }
 
-    return (dest);
+return (dest);
 }
 
