1bf987fe62352cdc6d7321d63e61fbd96f56890a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 14:30:50 +0300

    fix

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,11 +7,11 @@
  * Fills the first n bytes of the memory
  * area pointed to by s with the constant byte b.
  *
- * s: a pointer to the memory area to be filled
- * b:the constant byte to fill the memory area with
+ *s: a pointer to the memory area to be filled
+ *@b: the constant byte to fill the memory area with
  * n:the number of bytes to fill
  *
- * return :a pointer to the memory area s
+ *@return :a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
