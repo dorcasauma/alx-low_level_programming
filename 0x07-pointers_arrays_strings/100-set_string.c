160302d09d96019a3a9093590d0a6599d8c0fa22
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 23:12:23 +0300

    009

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+/**
+ * set_string - Sets the value of a pointer to a char.
+ *
+ * @s:a pointer to a pointer to a char
+ * @to: a pointer to a char containing the value to set
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
