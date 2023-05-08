25f61e5df23500408f2adae0ab0cb704f74c200c
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 14:08:21 +0300

    007

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b.
+ *
+ * @param s A pointer to the memory area to be filled
+ * @param b The constant byte to fill the memory area with
+ * @param n The number of bytes to fill
+ *
+ * @return A pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned char *ptr = (unsigned char*) s;
+for (unsigned int i = 0; i < n; i++)
+{
+ptr[i] = (unsigned char) b;
+}
+return (s);
+}
+
