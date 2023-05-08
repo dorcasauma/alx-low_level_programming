7cf63d0d7a56f527cc843d1c97499c4c603b2cf9
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 20:43:32 +0300

    Ggi

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+/**
+ * _strs - Calculates the length of the
+ * initial segment of the string 's' that consists of
+ * characters in the string 'accept'.
+ *
+ * @param s: A pointer to a null-terminated string.
+ * @param accept: A pointer to a null-terminated string.
+ * Return: The number of bytes in the initial segment of s which consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+break;
+}
+}
+if (accept[j] == '\0')
+{
+return (i);
+}
+}
+return (i);
+}
