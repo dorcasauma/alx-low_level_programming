de906ce6ef83a25e741d88302ee11db06d40b522
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 22:54:30 +0300

    008 commit

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+/**
+ * print_diagsums - Prints the sum of the two diagonals of a square matrix of integers.
+ *
+ * @a: a pointer to the first element of the square matrix
+ * @size:the size of one side of the square matrix
+ */
+void print_diagsums(int *a, int size)
+{
+int i, j, sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += *(a + i*size + i);
+sum2 += *(a + i*size + (size-1-i));
+printf("Diagonal sum 1: %d\n", sum1);
+printf("Diagonal sum 2: %d\n", sum2);
+}
+}
