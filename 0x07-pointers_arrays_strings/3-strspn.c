68366826a8b429e2c52d988763bc17b56c1aaac4
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 21:57:35 +0300

    fix

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,14 @@
 /**
- * This fuction gets the length of a prefix substring.
- * __strspn : Calculates the length of the
+ * __strspn - Calculates the length of the
  * initial segment of the string 's' that consists of
  * characters in the string 'accept'.
  *
  * @s: A pointer to a null-terminated string.
  * @accept: A pointer to a null-terminated string.
- * Return: The number of bytes in the initial segment of s which consist only of bytes from accept.
+ * Return: The number of bytes in the initial
+ * segment of s which consist only of bytes from accept.
  */
+
 unsigned int _strspn(char *s, char *accept)
 {
 unsigned int i, j;
