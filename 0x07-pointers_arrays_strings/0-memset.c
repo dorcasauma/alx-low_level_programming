ea1054d6c5904042833e1acdb6f004cbe22d2398
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 14:12:54 +0300

    007

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,7 +14,8 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned char *ptr = (unsigned char*) s;
-for (unsigned int i = 0; i < n; i++)
+unsigned int i;
+for (i = 0; i < n; i++)
 {
 ptr[i] = (unsigned char) b;
 }
