10d7ac04c66da24f4acfae6a6158f06f0ed4271a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 22:32:55 +0300

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+/**
+ * print_chessboard - Prints a chessboard represented
+ * by a 2D array of characters.
+ *
+ * @a - A pointer to an 8x8 2D array of characters
+ * representing the chessboard.
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+printf("%c ", a[i][j]);
+}
+printf("\n");
+}
+}
