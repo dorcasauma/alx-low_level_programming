bb14b91d5c40cada06838311280f5ee94aa33704
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 15:16:50 +0300

    betty style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,11 +4,11 @@
 /**
  * _memcpy - Copies n bytes from memory area src to memory area dest.
  *
- * @dest A pointer to the destination memory area
- * @src A pointer to the source memory area
- * @n The number of bytes to copy
+ * @dest: A pointer to the destination memory area
+ * @src: A pointer to the source memory area
+ * @n: The number of bytes to copy
  *
- * @return A pointer to the destination memory area
+ * Return: A pointer to the destination memory area
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
