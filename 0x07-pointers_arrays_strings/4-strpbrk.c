5c448509c6d218b2a3c83d40fc8d78108573e175
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-08 22:14:23 +0300

    fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,10 @@
 /**
- * *_strpbrk - Locates the first occurrence in
+ * _strpbrk - Locates the first occurrence in
  * the string 's' of any of the bytes in the string 'accept'.
  *
- * @param s: A pointer to a null-terminated string.
- * @param accept: A pointer to a null-terminated string.
- * Return: A pointer to the byte in s that matches one of the bytes in accept,
+ * @s: A pointer to a null-terminated string.
+ * @accept: a pointer to a null-terminated string.
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
  * or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
