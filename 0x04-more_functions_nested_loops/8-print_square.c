9b177dea2882faf5079278b25cbd5365ae0f2d84
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-09 22:12:24 +0300

    corrections

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+/**
+ * print_square - Prints a square of a given size using '#'
+ * @size: The size of the square to be printed
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+int row, col;
+
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+
+for (row = 0; row < size; row++)
+{
+for (col = 0; col < size; col++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
