0b83b7d25be673aa3d31630e7e7a503338a96cba
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-09 19:10:31 +0300

    corrections

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ */
+void jack_bauer()
+{
+for (int hour = 0; hour < 24; hour++)
+{
+for (int minute = 0; minute < 60; minute++)
+{
+printf("%02d:%02d\n", hour, minute);
+}
+}
+}
+
