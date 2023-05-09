365dd24578c9b7d522b39483b820ae6303d52219
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-09 19:33:38 +0300

    corrections

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+unsigned long int a = 1, b = 2, c;
+int i;
+
+printf("%lu, %lu", a, b);
+
+for (i = 2; i < 98; i++)
+{
+c = a + b;
+printf(", %lu", c);
+a = b;
+b = c;
+}
+
+printf("\n");
+
+return (0);
+}
