f5aec653139d47089d44c9d77312145d79aaa92e
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-09 19:53:25 +0300

    corrections

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints first
+ * 50 Fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+long int fib1 = 1, fib2 = 2, next_fib;
+
+printf("%ld, %ld", fib1, fib2);
+
+for (i = 2; i < 50; i++)
+{
+next_fib = fib1 + fib2;
+printf(", %ld", next_fib);
+
+fib1 = fib2;
+fib2 = next_fib;
+}
+
+printf("\n");
+return (0);
+}
