e12d0e2764bc81f72bce02311bb7100b99554ffb
DORCAS AUMA <dorcasauma89@gmail.com>
2023-05-09 19:27:03 +0300

    corrections add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+
+* add - Adds two integers and returns the result
+* @a: The first integer to add
+* @b: The second integer to add
+* Return: The sum of the two integers
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
