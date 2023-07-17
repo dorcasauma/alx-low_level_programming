a65eabdcf22d5c49d361cc3dce5c841b5dc62bac
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-17 14:54:14 +0300

    fix

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,10 +1,3 @@
 #include <stdio.h>
 
 #define SUM(x, y) ((x) + (y))
-
-int main() {
-    int num1 = 5;
-    int num2 = 3;
-    printf("Sum of %d and %d is %d\n", num1, num2, SUM(num1, num2));
-    return 0;
-}
