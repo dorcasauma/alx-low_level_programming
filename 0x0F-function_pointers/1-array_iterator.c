eb2bab3243265768e7b46b531b604396d04e0124
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-19 16:28:33 +0300

    nn

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,8 +5,9 @@
  * function pointer as
  * arguments. It then executes the provided function
  * on each element of the array.
- * print_element - This fuction Prints an integer element to the standard output.
- * @param element - The integer element to be printed
+ * print_element This fuction Prints an integer element to the 
+ * standard output.
+ * @param element The integer element to be printed
  * @param array Pointer to the integer array.
  * @param size The size of the array.
  * @param action A function pointer to a function that takes an integer as
