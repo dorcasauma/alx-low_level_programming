33e10699d4b5b1f7cb96d913a15edfee539f3a90
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-19 16:09:32 +0300

    fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,7 +4,8 @@
  * @brief Executes the provided function on each element of an integer array.
  *
  * This function takes an integer array, its size, and a function pointer as
- * arguments. It then executes the provided function on each element of the array.
+ * arguments. It then executes the provided function
+ * on each element of the array.
  *
  * @param array Pointer to the integer array.
  * @param size The size of the array.
@@ -20,7 +21,7 @@ printf("%d ", element);
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
-for ( i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
 action(array[i]);
 }
