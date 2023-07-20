ec92361c2fff4583b28effcbe562712943c52e2d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-20 16:26:10 +0300

    fix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdarg.h>
+unsigned int i; // Move the variable declaration here
+int num;
 void print_numbers(const char *separator, const unsigned int n, ...) {
     va_list args;
     va_start(args, n);
-
-    unsigned int i; // Move the variable declaration here
-    int num; // Move the variable declaration here
 
     for (i = 0; i < n; i++) {
         num = va_arg(args, int);
