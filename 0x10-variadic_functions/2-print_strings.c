f36f45921f0ef433779eb3c60855a71e97296464
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-20 17:53:01 +0300

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,9 +2,9 @@
 #include <stdarg.h>
 
 void print_strings(const char *separator, const unsigned int n, ...) {
+unsigned int i;
 va_list args;
 va_start(args, n);
-unsigned int ;
 
 for (i = 0;i < n; i++)
 {
