cfa3b5f72ed3eef65b070d91e184be4305e1654d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-20 15:11:20 +0300

    variadic fuctions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+if (n == 0)
+return 0;
+
+int sum = 0;
+
+va_list args;
+va_start(args, n);
+
+for (unsigned int i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+
+va_end(args);
+
+return (sum);
+}
