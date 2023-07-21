75fd5ac1c53ecd5b2617fce988bc308eb8a32719
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-21 13:06:46 +0300

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,14 +1,16 @@
 /**
- * Searches for an integer in an array and returns the index of the first element
+ * int_index - Searches for an integer in an array and 
+ * returns the index of the first element
  * for which the provided comparison function does not return 0.
  *
- * @param array The pointer to the integer array to search.
- * @param size The number of elements in the array.
- * @param cmp A pointer to the function used to compare values.
- *            The comparison function should take an integer argument and return an integer.
- *            It should return 0 if the comparison fails, and a non-zero value otherwise.
+ * @array: The pointer to the integer array to search.
+ * @size: The number of elements in the array.
+ * @cmp: A pointer to the function used to 
+ * compare values.
+ * The comparison function should take an integer argument and return an integer.
+ * Return: int_index should return 0 if the comparison fails, and a non-zero value otherwise.
  *
- * @return The index of the first element that matches the comparison function.
+ * Return: The index of the first element that matches the comparison function.
  *         If no element matches, or if size <= 0, it returns -1.
  */
 int int_index(int *array, int size, int (*cmp)(int))
