22e67024382e6cd7b2b9b5466cdb7fc5dd0ab740
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-26 05:08:24 +0300

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog -  Represents a dog with its name, age, and owner information.
+ *struct dog -  Represents a dog with its name, age, and owner information.
  */
 
 struct dog
