42fd08bbef0167e4454642d66915d9df9edde175
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-26 08:35:49 +0300

    fix

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * @brief Initializes a dog structure with the provided information.
+ *
+ * init_dog - This function takes a pointer to a struct dog, along with the dog's name, age, and owner
+ * information, and initializes the dog structure with the given values.
+ *
+ * @param d: Pointer to the struct dog variable to be initialized.
+ * @param name: Pointer to the character array containing the name of the dog.
+ * @param age: The age of the dog.
+ * @param owner: Pointer to the character array containing the name of the dog's owner.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = strdup(name);
+d->age = age;
+d->owner = strdup(owner);
+}
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * @brief Prints the contents of a dog structure.
+ *
+ * print_dog - This function takes a pointer to a `struct dog` and prints its contents, including the name,
+ * age, and owner information. If any element of the `struct dog` is NULL, it will print "(nil)"
+ * instead of the NULL value.
+ *
+ * @param d: Pointer to the `struct dog` variable to be printed.
+ *
+ * @note If `d` is NULL, the function will print nothing.
+ */
+
+
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.1f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
