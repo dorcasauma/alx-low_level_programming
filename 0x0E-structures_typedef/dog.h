445a7dab25890ea31d0f0223587c1d18a479033e
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-26 16:21:23 +0300

    ff

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 /**
+ * struct dog_t - Typedef for the struct dog type.
  * struct dog -  represents a dog with its name, age, and owner information.
  *@name: the name of the dog.
  *@age: the age of the dog.
