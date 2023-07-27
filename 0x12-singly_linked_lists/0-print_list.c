bebc2e8dd79310281c9a3d7d9518b085677a36ec
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-27 13:03:42 +0300

    ff

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,30 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+typedef struct Node
+{
+char *str;
+size_t len;
+struct Node *next;
+}
+list_t;
+size_t print_list(const list_t *h)
+{
+size_t node_count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
+} else
+{
+printf("[%lu] %s\n", node_count, h->str);
+}
 
-// Define the structure for the linked list node
-typedef struct Node {
-    char *str;
-    size_t len;
-    struct Node *next;
-} list_t;
+node_count++;
+h = h->next;
+}
 
-// Function to print all elements of the linked list and return the number of nodes
-size_t print_list(const list_t *h) {
-    size_t node_count = 0; // Counter to keep track of the number of nodes
-
-    // Traverse the linked list and print each element
-    while (h != NULL) {
-        // If the string is NULL, print "[0] (nil)"
-        if (h->str == NULL) {
-            printf("[0] (nil)\n");
-        } else {
-            printf("[%lu] %s\n", node_count, h->str);
-        }
-
-        node_count++; // Increment the node counter
-        h = h->next;  // Move to the next node
-    }
-
-    return node_count; // Return the number of nodes
+return node_count;
 }
