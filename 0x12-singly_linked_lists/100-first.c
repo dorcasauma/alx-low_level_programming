d3afbc80d2573e864d39b684a62debd32202bf8b
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-27 16:01:27 +0300

    h

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -12,5 +12,5 @@ void print_message(void) __attribute__((constructor));
 void print_message(void)
 {
 printf("You're beat! and yet, you must allow,\n"
- "I bore my house upon my back!\n");
+"I bore my house upon my back!\n");
 }
