572bab04e19c1cabeb4478c0a4dedf5643aad151
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-27 16:00:45 +0300

    xxx

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+/**
+ * print_message - Prints a message before the main function is executed.
+ *
+ * Description: This function is marked with the constructor attribute,
+ *              which means it will run automatically before the main function.
+ *              It prints the specified message to the standard output.
+ */
 
 void print_message(void) __attribute__((constructor));
 
 void print_message(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+ "I bore my house upon my back!\n");
 }
