8281afe13519b3540e6c9baec1f99b89e2ce8487
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-27 13:25:34 +0300

    hhh

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -13,14 +13,10 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
-#endif /* LISTS_H */
-
-
-
-
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+#endif /* LISTS_H */
