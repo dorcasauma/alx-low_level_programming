e8bf99a813843cb290759a9464fd76396efb6553
DORCAS AUMA <dorcasauma89@gmail.com>
2023-07-27 13:27:58 +0300

    kjjj

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+/**
+ * list_len - Returns the number of elements in a
+ * linked list_t list.
+ * @h: Pointer to the head (start) of the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
 size_t list_len(const list_t *h)
 {
 size_t node_count = 0;
