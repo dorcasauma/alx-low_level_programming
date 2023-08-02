cf57ef577f6420e4ceeacb15acd21b184ab6e427
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 15:36:34 +0300

    ggg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,12 @@
+/**
+ * main -  The main function is the entry point of the program.
+ *        It prints the name of the program (argv[0]) to the console.
+ *
+ * @argc: The number of command-line arguments, including the program name.
+ * @ argv: An array of strings representing the command-line arguments.
+ *
+ * Return: The function returns 0 to indicate successful program execution.
+ */
 #include <stdio.h>
 
 int main(int argc, char *argv[])
