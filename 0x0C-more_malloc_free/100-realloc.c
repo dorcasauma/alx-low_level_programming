db630d20cbcc5721a0094457228ba587c3f0e91b
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 13:09:35 +0300

    fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,9 +8,10 @@
  * @new_size: New size, in bytes, of the new memory block.
  *
  * Return: A pointer to the newly reallocated memory block.
- *         If new_size is equal to zero and ptr is not NULL, the function returns NULL.
- *         If malloc fails, the function returns NULL.
- *         If ptr is NULL, the call is equivalent to malloc(new_size).
+ *  If new_size is equal to zero and ptr is not NULL,
+ *  the function returns NULL.
+ *  If malloc fails, the function returns NULL.
+ *  If ptr is NULL, the call is equivalent to malloc(new_size).
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
@@ -25,14 +26,14 @@ if (ptr == NULL)
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 return (NULL);
-return new_ptr;
+return (new_ptr);
 }
 if (new_size == old_size)
-return ptr;
+return (ptr);
 new_ptr = malloc(new_size);
 if (new_ptr == NULL)
 return (NULL);
 memcpy(new_ptr, ptr, (old_size < new_size) ? old_size : new_size);
 free(ptr);
-return new_ptr;
+return (new_ptr);
 }
