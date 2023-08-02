857a4c5ec4520fb155fa3fb193e189b1b09f8345
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 12:50:21 +0300

    kjj

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,18 +12,24 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr = NULL;
-unsigned int total_size;
-unsigned int i;
-unsigned char *char_ptr = ptr;
+    void *ptr;
+    unsigned int total_size;
+    unsigned char *char_ptr;
+    unsigned int i;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
-total_size = nmemb * size;
-ptr = malloc(total_size);
-if (ptr == NULL)
-return (NULL);
-for ( i = 0; i < total_size; i++)
-char_ptr[i] = 0;
-return (ptr);
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    total_size = nmemb * size;
+    ptr = malloc(total_size);
+
+    if (ptr == NULL)
+        return NULL;
+
+    // Initialize the allocated memory to zero
+    char_ptr = ptr;
+    for (i = 0; i < total_size; i++)
+        char_ptr[i] = 0;
+
+    return ptr;
 }
