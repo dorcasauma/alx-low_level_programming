31676e4d7f78ae262b8df975ca700863c460c00d
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 14:04:34 +0300

    GFG

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,51 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-// Function to check if a string is a positive integer
-int is_positive_integer(const char *str) {
-    int i;
-    for (i = 0; str[i] != '\0'; i++) {
-        if (!isdigit(str[i])) {
-            return 0;
-        }
-    }
-    return 1;
+int is_positive_integer(const char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (!isdigit(str[i]))
+{
+return (0);
+}
+}
+return (1);
+}
+unsigned long int multiply_positive_integers(unsigned long int num1, unsigned long int num2)
+{
+return (num1 * num2);
+}
+int main(int argc, char *argv[])
+{
+char *endptr;
+unsigned long int num1, num2, result;
+if (argc != 3)
+{
+fprintf(stderr, "Error\n");
+return (98);
+}
+num1 = strtoul(argv[1], &endptr, 10);
+if (*endptr != '\0' || !is_positive_integer(argv[1]))
+{
+fprintf(stderr, "Error\n");
+return 98;
+}
+num2 = strtoul(argv[2], &endptr, 10);
+if (*endptr != '\0' || !is_positive_integer(argv[2]))
+{
+fprintf(stderr, "Error\n");
+return (98);
+}
+result = multiply_positive_integers(num1, num2);
+printf("%lu\n", result);
+return (0);
 }
 
-// Function to multiply two positive integers
-unsigned long int multiply_positive_integers(unsigned long int num1, unsigned long int num2) {
-    return num1 * num2;
-}
-
-int main(int argc, char *argv[]) {
-    char *endptr;
-    unsigned long int num1, num2, result; // Declare all variables at the beginning
-
-    // Check if the number of arguments is correct
-    if (argc != 3) {
-        fprintf(stderr, "Error\n");
-        return 98;
-    }
-
-    // Parse the command-line arguments and check if they are valid positive integers
-    num1 = strtoul(argv[1], &endptr, 10);
-    if (*endptr != '\0' || !is_positive_integer(argv[1])) {
-        fprintf(stderr, "Error\n");
-        return 98;
-    }
-
-    num2 = strtoul(argv[2], &endptr, 10);
-    if (*endptr != '\0' || !is_positive_integer(argv[2])) {
-        fprintf(stderr, "Error\n");
-        return 98;
-    }
-
-    // Perform the multiplication and store the result
-    result = multiply_positive_integers(num1, num2);
-
-    // Print the result
-    printf("%lu\n", result);
-
-    return 0;
-}
