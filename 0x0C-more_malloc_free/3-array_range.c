b19dbb14a6dfea9843465bfd821554f280349fee
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 12:58:55 +0300

    aa

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -10,23 +10,18 @@
  */
 int *array_range(int min, int max)
 {
-    int *arr;
-    int size, i;
-
-    if (min > max)
-        return NULL;
-
-    size = max - min + 1;
-
-    arr = (int *)malloc(size * sizeof(int));
-    if (arr == NULL)
-        return NULL;
-
-    for (i = 0; i < size; i++)
-    {
-        arr[i] = min;
-        min++;
-    }
-
-    return arr;
+int *arr;
+int size, i;
+if (min > max)
+return (NULL);
+size = max - min + 1;
+arr = (int *)malloc(size * sizeof(int));
+if (arr == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+{
+arr[i] = min;
+min++;
+}
+return (arr);
 }
