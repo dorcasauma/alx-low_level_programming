7782226ba669b4593376781f457b23b47b02981e
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 14:17:46 +0300

    commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - Prints a string in reverse using recursion.
+ * @s: A pointer to the input string.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+        return;
+_print_rev_recursion(s + 1);
+putchar(*s);
+}
