bf39e29037431521ab0fd131b5b5e40772a2893b
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 15:05:57 +0300

    paren

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,8 +11,8 @@ int is_prime_helper(int n, int i)
 if (i * i > n)
 return (1);
 if (n % i == 0)
-return 0;
-return is_prime_helper(n, i + 1);
+return (0);
+return (is_prime_helper(n, i + 1));
 }
 
 /**
@@ -23,6 +23,6 @@ return is_prime_helper(n, i + 1);
 int is_prime_number(int n)
 {
 if (n <= 1)
-return 0;
-return is_prime_helper(n, 2);
+return (0);
+return (is_prime_helper(n, 2));
 }
