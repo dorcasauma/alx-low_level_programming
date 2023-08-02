a92caa7dac205f86a5b0537e44d928f224b47ea3
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 15:51:37 +0300

    MULL

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,12 +16,12 @@ int main(int argc, char *argv[])
 int num1, num2, result;
 if (argc != 3)
 {
-printf("Error: Usage: %s <num1> <num2>\n", argv[0]);
+printf("Error\n");
 return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-result = num1 * num2;
+result = num1 *num2;
 printf("%d\n", result);
 return (0);
 }
