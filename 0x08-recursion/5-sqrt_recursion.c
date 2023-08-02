f108bb628409c45fdb16ccbf134e08f5c320c02a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 14:43:42 +0300

    bb

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,8 @@
  * @n: The number for which to find the square root.
  * @start: The starting value to check for the square root.
  * @end: The ending value to check for the square root.
- * Return: The natural square root of n, or -1 if it doesn't have a natural square root.
+ * Return: The natural square root of n, or -1 if it doesn't have a 
+ * natural square root.
  */
 int _sqrt_helper(int n, int start, int end)
 {
@@ -23,13 +24,14 @@ return (_sqrt_helper(n, start, mid - 1));
 /**
  * _sqrt_recursion - Returns the natural square root of a number.
  * @n: The number for which to find the square root.
- * Return: The natural square root of n, or -1 if it doesn't have a natural square root.
+ * Return: The natural square root of n, or -1 if it doesn't
+ * have a natural square root.
  */
 int _sqrt_recursion(int n)
 {
 int start = 0;
 int end = n;
 if (n < 0)
-return -1;
+return (-1);
 return (_sqrt_helper(n, start, end));
 }
