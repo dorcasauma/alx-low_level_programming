2b585b59a9502f0b472c2afe8cf338612b4793cb
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 15:39:42 +0300

    description

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-
+/**
+ * main - The main function is the entry point of the program.
+ *        It prints the number of command-line arguments (excluding the program name) to the console.
+ *
+ * @argc: The number of command-line arguments, including the program name.
+ * @argv: An array of strings representing the command-line arguments.
+ *
+ * Return: The function returns 0 to indicate successful program execution.
+ */
 int main(int argc, char *argv[])
 {
 (void)argv;
