165a2138eb3abff1d7b71c78c1d22b38ba22397f
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 13:58:03 +0300

    BVV

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <ctype.h>
 
 // Function to check if a string is a positive integer
-bool is_positive_integer(const char *str) {
-    for (int i = 0; str[i] != '\0'; i++) {
+int is_positive_integer(const char *str) {
+    int i;
+    for (i = 0; str[i] != '\0'; i++) {
         if (!isdigit(str[i])) {
-            return false;
+            return 0;
         }
     }
-    return true;
+    return 1;
 }
 
 // Function to multiply two positive integers
@@ -19,6 +19,9 @@ unsigned long int multiply_positive_integers(unsigned long int num1, unsigned lo
 }
 
 int main(int argc, char *argv[]) {
+    char *endptr;
+    unsigned long int num1, num2, result; // Declare all variables at the beginning
+
     // Check if the number of arguments is correct
     if (argc != 3) {
         fprintf(stderr, "Error\n");
@@ -26,21 +29,22 @@ int main(int argc, char *argv[]) {
     }
 
     // Parse the command-line arguments and check if they are valid positive integers
-    char *endptr;
-    unsigned long int num1 = strtoul(argv[1], &endptr, 10);
+    num1 = strtoul(argv[1], &endptr, 10);
     if (*endptr != '\0' || !is_positive_integer(argv[1])) {
         fprintf(stderr, "Error\n");
         return 98;
     }
 
-    unsigned long int num2 = strtoul(argv[2], &endptr, 10);
+    num2 = strtoul(argv[2], &endptr, 10);
     if (*endptr != '\0' || !is_positive_integer(argv[2])) {
         fprintf(stderr, "Error\n");
         return 98;
     }
 
-    // Perform the multiplication and print the result
-    unsigned long int result = multiply_positive_integers(num1, num2);
+    // Perform the multiplication and store the result
+    result = multiply_positive_integers(num1, num2);
+
+    // Print the result
     printf("%lu\n", result);
 
     return 0;
