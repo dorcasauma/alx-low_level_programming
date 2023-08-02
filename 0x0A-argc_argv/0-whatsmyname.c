e2a4f236847a1f4bac32e48090ef688a4faef22a
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-02 15:37:25 +0300

    jhh

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,7 +3,7 @@
  *        It prints the name of the program (argv[0]) to the console.
  *
  * @argc: The number of command-line arguments, including the program name.
- * @ argv: An array of strings representing the command-line arguments.
+ * @argv: An array of strings representing the command-line arguments.
  *
  * Return: The function returns 0 to indicate successful program execution.
  */
