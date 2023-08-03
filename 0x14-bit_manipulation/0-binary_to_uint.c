869770e27af747029b9df0a815fe260bc1a6004f
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-03 15:10:56 +0300

    lll

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b) {
+    unsigned int result = 0;
+    if (b == NULL)
+        return 0;
+
+
+    while (*b != '\0') {
+        char c = *b;
+
+        if (c != '0' && c != '1')
+            return 0;
+
+        result = (result << 1) + (c - '0');
+        b++;
+    }
+
+    return result;
+}
+
