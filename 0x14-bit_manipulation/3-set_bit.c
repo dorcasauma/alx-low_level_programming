fb7df82dd521fbfe627ea2a5d0840d3c8cda1ce9
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-03 17:48:37 +0300

    f

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,12 +5,14 @@
  * @n: A pointer to the number in which the bit is to be set.
  * @index: The index of the bit to be set (starting from 0).
  *
- * Return: 1 if it worked, or -1 if an error occurred (e.g., if index is out of range).
+ * Return: 1 if it worked, or -1 if an error occurred
+ *  (e.g., if index is out of range).
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int mask = 1UL << index;
-if (index >= sizeof(unsigned long int) * 8) {
+if (index >= sizeof(unsigned long int) * 8)
+{
 return (-1);
 }
 *n |= mask;
