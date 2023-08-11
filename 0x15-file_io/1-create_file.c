6960a20cf6bdfaff4643d3efb3ca6a08bab25a18
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-11 15:19:06 +0300

    nb

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int create_file(const char *filename, char *text_content); {
-FILE *file;
-file; fopen("file.txt");
-fclose();
-return (0);
+int create_file(const char *filename, char *text_content) {
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        return -1;
+    }
+
+    if (text_content != NULL) {
+        fprintf(file, "%s", text_content);
+    }
+
+    fclose(file);
+    return 0;
 }
+
