6427ed45757a26ccb0961886649bd69aefc8272e
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-17 16:38:43 +0300

    read and writting a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-ssize_t read_textfile(const char *filename, size_t letters){
-    char *ch = (char *)malloc(letters + 1);
-    ssize_t total_bytes_read = 0;
-
-    FILE *file = fopen(filename, "r");
+ssize_t read_textfile(const char *filename)
+{
+    FILE* myfile;
+    char buffer[101];
     if (filename == NULL){
-        return 0;
+        return (0);
     }
-    while (fgets(ch,letters,file) != NULL){
-        printf("%s", ch);
-        total_bytes_read += strlen(ch);
+    myfile = fopen(filename, "r");
+    if (myfile == NULL){
+        return (0);
+    printf("%s \n",fgets(buffer,sizeof(buffer),myfile));
+    }
+    fclose(myfile);
+    return strlen(buffer);
 
-        if ((size_t)total_bytes_read >= letters) {
-            break; // Stop reading if the desired number of letters is reached
-        }
-    }
-    printf("%s\n", ch);
-    fclose(file);
-    return total_bytes_read;
 }
