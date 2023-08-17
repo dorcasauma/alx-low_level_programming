ffaf4c3d695df1873280a9436c874516c6f9cbb7
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-17 17:11:37 +0300

    betyyy

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * create_file - creates a file
+ * @text_content: terminated string to write to the file
+ * @filename: file to be created
+ * Return:1 on success,-1 on failure
+*/
 
 int create_file(const char *filename, char *text_content)
 {
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        return -1;
-    }
+FILE *file = fopen(filename, "w");
+if (file == NULL)
+{
+return -1;
+}
+if (text_content != NULL)
+{
+fprintf(file, "%s", text_content);
+}
 
-    if (text_content != NULL) {
-        fprintf(file, "%s", text_content);
-    }
-
-    fclose(file);
-    return (1);
+fclose(file);
+return (1);
 }
 
