6dcec1a69984647ba99295c6d55e49c7edfa06da
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-17 16:44:51 +0300

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * read_textfile - Reads a textfile
+ * @myfile: file to be read
+ * Return: amount of bytes
+*/
 ssize_t read_textfile(const char *filename)
 {
-    FILE* myfile;
-    char buffer[101];
-    if (filename == NULL){
-        return (0);
-    }
-    myfile = fopen(filename, "r");
-    if (myfile == NULL){
-        return (0);
-    printf("%s \n",fgets(buffer,sizeof(buffer),myfile));
-    }
-    fclose(myfile);
-    return strlen(buffer);
+FILE* myfile;
+char buffer[101];
+if (filename == NULL)
+{
+return (0);
+}
+myfile = fopen(filename, "r");
+if (myfile == NULL)
+{
+return (0);
+printf("%s \n",fgets(buffer, sizeof(buffer) ,myfile));
+}
+fclose(myfile);
+return strlen(buffer);
 
 }
