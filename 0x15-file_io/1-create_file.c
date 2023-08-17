fe7c604aba4ffc39d2d489f2812957f61f821011
DORCAS AUMA <dorcasauma89@gmail.com>
2023-08-17 17:03:45 +0300

    edit return

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
     FILE *file = fopen(filename, "w");
     if (file == NULL) {
         return -1;
@@ -12,6 +13,6 @@ int create_file(const char *filename, char *text_content) {
     }
 
     fclose(file);
-    return 0;
+    return (1);
 }
 
