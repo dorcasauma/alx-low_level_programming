425bf70613bd7b5e10fe2c4a954f52537df3a019
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-21 14:03:05 +0300

    fix_betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int append_text_to_file(const char *filename, char *text_content) {
-    FILE *file = fopen(filename, "a"); // Open the file in append mode
-
-    if (filename == NULL) {
-        return -1;
-    }
-
-    if (file == NULL) {
-        return -1; // File does not exist or permission issue
-    }
-
-    if (text_content != NULL) {
-        fprintf(file, "%s", text_content);
-    }
-
-    fclose(file);
-
-    return 1;
+int append_text_to_file(const char *filename, char *text_content)
+{
+FILE *file = fopen(filename, "a");
+if (filename == NULL)
+{
+return (-1);
+}
+if (file == NULL)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+fprintf(file, "%s", text_content);
+}
+fclose(file);
+return(1);
 }
