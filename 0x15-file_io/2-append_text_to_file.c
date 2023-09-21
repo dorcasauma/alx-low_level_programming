c9c7f17b00d84d026a29235d9eab2a7568a48fc6
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-21 14:04:57 +0300

    fix_betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * append_text_to_file - Appends text content to an existing file.
+ *
+ * @filename: The name of the file to which text content should be appended.
+ * @text_content: The text content to be appended to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -17,5 +25,5 @@ if (text_content != NULL)
 fprintf(file, "%s", text_content);
 }
 fclose(file);
-return(1);
+return (1);
 }
