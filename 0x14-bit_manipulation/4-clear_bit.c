2130ea4b93f0761a97d18fff8785a4c6c35af9a8
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-21 11:53:25 +0300

    description

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * clear_bit - Clear a bit at a specified index.
+ *
+ * @n: A pointer to an unsigned long int.
+ * @index: The index of the bit to clear.
+ *
+ * Return: 1 if successful, or -1 if an error occurs.
+ */
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
