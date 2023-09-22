f25a134a5b6bd7dffb8b7b2136f5ea531f48dbdd
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-22 13:27:41 +0300

    fix betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,48 +5,46 @@
 
 #define BUFSIZE 1024
 
-void error_exit(int code, const char *message) {
-    dprintf(STDERR_FILENO, "Error: %s\n", message);
-    exit(code);
+void error_exit(int code, const char *message)
+{
+dprintf(STDERR_FILENO, "Error: %s\n", message);
+exit(code);
 }
 
-int main(int argc, char *argv[]) {
-    int fd_from, fd_to;
-    ssize_t bytes_read, bytes_written;
-    char buffer[BUFSIZE];
-
-    if (argc != 3) {
-        error_exit(97, "Usage: cp file_from file_to");
-    }
-
-    fd_from = open(argv[1], O_RDONLY);
-    if (fd_from == -1) {
-        error_exit(98, "Can't read from file");
-    }
-
-    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (fd_to == -1) {
-        error_exit(99, "Can't write to file");
-    }
-
-    while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0) {
-        bytes_written = write(fd_to, buffer, bytes_read);
-        if (bytes_written == -1 || bytes_written != bytes_read) {
-            error_exit(99, "Can't write to file");
-        }
-    }
-
-    if (bytes_read == -1) {
-        error_exit(98, "Can't read from file");
-    }
-
-    if (close(fd_from) == -1) {
-        error_exit(100, "Can't close fd");
-    }
-
-    if (close(fd_to) == -1) {
-        error_exit(100, "Can't close fd");
-    }
-
-    return 0;
+int main(int argc, char *argv[])
+{
+int fd_from, fd_to;
+ssize_t bytes_read, bytes_written;
+char buffer[BUFSIZE];
+if (argc != 3)
+{
+error_exit(97, "Usage: cp file_from file_to");
+}
+fd_from = open(argv[1], O_RDONLY);
+if (fd_from == -1)
+{
+error_exit(98, "Can't read from file");
+}
+fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (fd_to == -1)
+{
+error_exit(99, "Can't write to file");
+}
+while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0)
+{
+bytes_written = write(fd_to, buffer, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+{
+error_exit(99, "Can't write to file");
+}
+}
+if (bytes_read == -1)
+{
+error_exit(98, "Can't read from file");
+}
+if (close(fd_from) == -1 || close(fd_to) == -1)
+{
+error_exit(100, "Can't close fd");
+}
+return (0);
 }
