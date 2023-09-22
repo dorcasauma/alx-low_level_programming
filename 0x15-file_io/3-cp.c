094a81afcfaf6734d12c2422bb6433c11488282c
DORCAS AUMA <dorcasauma89@gmail.com>
2023-09-22 13:19:38 +0300

    fixing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,8 +5,9 @@
 
 #define BUFSIZE 1024
 
-void error_exit(int code, const char *message) {
+void error_exit(const char *message) {
     dprintf(STDERR_FILENO, "Error: %s\n", message);
+    exit(1);
 }
 
 int main(int argc, char *argv[]) {
@@ -15,42 +16,36 @@ int main(int argc, char *argv[]) {
     char buffer[BUFSIZE];
 
     if (argc != 3) {
-        error_exit(97, "Usage: cp file_from file_to");
+        error_exit("Usage: cp file_from file_to");
     }
 
     fd_from = open(argv[1], O_RDONLY);
     if (fd_from == -1) {
-        dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
-        exit(98);
+        error_exit("Can't read from file");
     }
 
     fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
     if (fd_to == -1) {
-        dprintf(STDERR_FILENO, "Can't write to file %s\n", argv[2]);
-        exit(99);
+        error_exit("Can't write to file");
     }
 
     while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0) {
         bytes_written = write(fd_to, buffer, bytes_read);
         if (bytes_written == -1 || bytes_written != bytes_read) {
-            dprintf(STDERR_FILENO, "Can't write to file %s\n", argv[2]);
-            exit(99);
+            error_exit("Can't write to file");
         }
     }
 
     if (bytes_read == -1) {
-        dprintf(STDERR_FILENO, "Can't read from file %s\n", argv[1]);
-        exit(98);
+        error_exit("Can't read from file");
     }
 
     if (close(fd_from) == -1) {
-        dprintf(STDERR_FILENO, "Can't close fd %d\n", fd_from);
-        exit(100);
+        error_exit("Can't close fd");
     }
 
     if (close(fd_to) == -1) {
-        dprintf(STDERR_FILENO, "Can't close fd %d\n", fd_to);
-        exit(100);
+        error_exit("Can't close fd");
     }
 
     return 0;
