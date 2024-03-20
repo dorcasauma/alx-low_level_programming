5cbe9c547af5c36bb411f5eba257465d90dc5bbb
DORCAS AUMA <dorcasauma89@gmail.com>
2024-03-20 21:35:16 +0300

    changes

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome to the School Program!\n");
+    printf("This is the main function.\n");
+    // Add your program logic here
+    return 0;
+}
